04192bf89864b34867f8bcb88e67abf2fcb3fcad
richa688 <task.stadsolution@gmail.com>
2026-06-23 19:10:08 +0530

    thcommit

--- TestngJenkin10amdemo/src/test/java/rinku/listnereg/Test.java
+++ TestngJenkin10amdemo/src/test/java/rinku/listnereg/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is new file");
+		System.out.println("try this file");
 
 	}
 
