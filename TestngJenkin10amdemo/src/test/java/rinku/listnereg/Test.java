83bf64575c51daf7b4834598dd5dc09cb432c53e
richa688 <task.stadsolution@gmail.com>
2026-06-23 19:07:26 +0530

    secommit

---
+++ TestngJenkin10amdemo/src/test/java/rinku/listnereg/Test.java
@@ -0,0 +1,11 @@
+package rinku.listnereg;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is new file");
+
+	}
+
+}
